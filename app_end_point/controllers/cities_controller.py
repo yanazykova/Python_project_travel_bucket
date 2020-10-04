378f932fbfebdbd554d9e9a77eea1d23ed8f339e
yanazykova <yvz1973@gmail.com>
2020-10-04 18:10:37 +0100

    Added CRUD to countries

--- app_end_point/controllers/cities_controller.py
+++ app_end_point/controllers/cities_controller.py
@@ -12,7 +12,46 @@ def cities():
     cities = city_repository.select_all()
     return render_template("cities/index.html", cities=cities)
 
-# SHOW??
-
 # NEW
+@cities_blueprint.route("/cities/new")
+def new_city():
+    countries = country_repository.select_all()
+    return render_template("cities/new.html", countries=countries)
+
+# CREATE
+@cities_blueprint.route("/cities", methods=["POST"])
+def create_city():
+    name = request.form["name"]
+    country_id = request.form["country_id"]
+    country = country_repository.select(country_id)
+    new_city = City(name, country)
+    city_repository.save(new_city)
+    return redirect("/cities")
+
+
+# EDIT
+@cities_blueprint.route("/cities/<id>/edit")
+def edit_city(id):
+    city = city_repository.select(id)
+    countries = country_repository.select_all()
+    return render_template('cities/edit.html', city=city, countries=countries)
+
+
+# UPDATE
+@cities_blueprint.route("/cities/<id>", methods=["POST"])
+def update_city(id):
+    name = request.form["name"]
+    country_id = request.form["country_id"]
+    country = country_repository.select(country_id)
+    city = City(name, country, id)
+    city_repository.update(city)
+    return redirect("/cities")
+
+
+# DELETE
+@cities_blueprint.route("/cities/<id>/delete", methods=["POST"])
+def delete_city(id):
+    city_repository.delete(id)
+    return redirect("/cities")
+
 
--- app_end_point/controllers/countries_controller.py
+++ app_end_point/controllers/countries_controller.py
@@ -3,13 +3,49 @@ from flask import Blueprint, Flask, redirect, render_template, request
 from models.country import Country
 import repositories.country_repository as country_repository
 
-countries_blueprint = Blueprint 
-("countries", __name__) 
+countries_blueprint = Blueprint("countries", __name__) 
 
 # INDEX
 @countries_blueprint.route("/countries")
 def countries():
     countries = country_repository.select_all()
     return render_template("countries/index.html", countries=countries)
+
+
+# NEW
+@countries_blueprint.route("/countries/new")
+def new_country():
+    return render_template("countries/new.html")
+
+
+# CREATE
+@countries_blueprint.route("/countries", methods=["POST"])
+def create_country():
+    name = request.form["name"]
+    new_country = Country(name)
+    country_repository.save(new_country)
+    return redirect("/countries")
+
+
+# EDIT
+@countries_blueprint.route("/countries/<id>/edit")
+def edit_country(id):
+    country = country_repository.select(id)
+    return render_template('countries/edit.html', country=country)
+
+
+# UPDATE
+@countries_blueprint.route("/countries/<id>", methods=["POST"])
+def update_country(id):
+    name = request.form["name"]
+    country = Country(name, id)
+    country_repository.update(country)
+
+
+# DELETE
+@countries_blueprint.route("/countries/<id>/delete", methods=["POST"])
+def delete_country(id):
+    country_repository.delete(id)
+    return redirect("/countries")
 
 
