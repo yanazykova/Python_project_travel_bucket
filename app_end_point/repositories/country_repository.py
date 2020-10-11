cb889e1d3d6acf22f9c7373e145299f93317954b
yanazykova <yvz1973@gmail.com>
2020-10-11 07:22:33 +0100

    Wireframes added

--- app_end_point/repositories/country_repository.py
+++ app_end_point/repositories/country_repository.py
@@ -9,13 +9,29 @@ def save(country):
     id = results[0]['id']
     country.id = id
 
+# Creating a function which will allow to select 
+# all the countries saved in the database and pass them to the index template:
 def select_all():
+    # Let's give a variable "countries" a value of an empty list:
     countries = []
+    # Let's assign a variable "sql" to the sql command
+    # which will select all countries from the countries table:
     sql = "SELECT * FROM countries"
+    # Variable "results" is assigned to the result of calling the 
+    # function "def run_sql" which was defined in the run_sql.py file, 
+    # in the brackets we're passing in the argument "sql" which is the 
+    # variable from the preseding assignment:
     results = run_sql(sql)
+    # now we're iterating over the dictionary which was returned 
+    # as the result of the invoking the above "run_sql" function:
     for result in results:
+    # the resulted dictionary would contain a key "country" for which we're 
+    # going to create an instance of a Country class with attributes "name" and "id":
         country = Country(result ["name"], result["id"])
+    # each returned country is now going to be appended to the forementioned 
+    # "countries" list:
         countries.append((country))
+    # and all the countries are going to be returned as a now completed list:
     return countries 
 
 def select(id):
